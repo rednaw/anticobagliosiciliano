12f8c19d5c4c6a3654f0d149666b2e3bfc5d2fa7
Wander Grevink <7900137+rednaw@users.noreply.github.com>
2026-08-18 11:00:39 +0000

    font preload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import figtree from '@fontsource-variable/figtree/files/figtree-latin-wght-normal.woff2?url';
+	import fraunces from '@fontsource-variable/fraunces/files/fraunces-latin-standard-normal.woff2?url';
 	import { site } from '$lib/data/content';
 	import { SITE_PUBLIC } from '$lib/site-config';
 
@@ -8,6 +10,8 @@
 </script>
 
 <svelte:head>
+	<link rel="preload" as="font" type="font/woff2" href={fraunces} crossorigin="anonymous" />
+	<link rel="preload" as="font" type="font/woff2" href={figtree} crossorigin="anonymous" />
 	<link rel="icon" href={favicon} />
 	{#if !SITE_PUBLIC}
 		<meta name="robots" content="noindex, nofollow" />
